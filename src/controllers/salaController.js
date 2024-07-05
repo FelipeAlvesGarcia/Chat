e5a0ab5198aae582d43a71232f779a71c1b06a4d
= <=>
2024-07-05 15:06:44 -0300

    etapa 3

--- src/controllers/salaController.js
+++ src/controllers/salaController.js
@@ -1,6 +1,5 @@
+const salaModel = require('../models/salaModel');
 
-exports.get=()=>{
-    let salaModel = require('../models/salaModel');
-    return salaModel.listarSalas();
-    
+exports.get= async()=>{
+    return await salaModel.listarSalas();   
 }
