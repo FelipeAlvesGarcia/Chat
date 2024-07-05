3b648f6956a32d490d69c6515e4d9f8860c9838a
= <=>
2024-07-05 17:08:44 -0300

    acabou a aula (etapa 1 do chat parte 3)

---
+++ src/controllers/usuarioController.js
@@ -0,0 +1,12 @@
+const token = require("../util/token");
+const ususaioModel = require("../models/usuarioModel.js");
+
+exports.entrar = async(nick)=>{
+    let resp =  await ususaioModel.registrarUsuario(nick);
+    if(resp.insertedId){
+        return {
+            "idUser":resp.insertedId,
+            "token": await token.setToken(JSON.stringify(resp.insertedId))
+        }
+    }
+}
